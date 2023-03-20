ba22677bd56d8404f56de06498946ff0dcea42f4
dan-vds <danielvds101@gmail.com>
2023-03-21 11:29:49 +1300

    Added comments to Profiles class

--- src/main/java/nz/ac/auckland/se281/Profiles.java
+++ src/main/java/nz/ac/auckland/se281/Profiles.java
@@ -1,20 +1,24 @@
 package nz.ac.auckland.se281;
 
 public class Profiles {
+  // Defining instance variables for the class Profiles
   private String username;
   private String age;
 
   public Profiles(String un, String a) {
+    // Defining constructor
     username = un;
     age = a;
   }
 
   public String getUsername() {
+    // Getter method to give access to usernames
     String name = username;
     return name;
   }
 
   public String getAge() {
+    // Getter method to give access to age
     String old = age;
     return old;
   }
