3aacf1f033f87f37ed466519195a9b2bed56ef92
dan-vds <danielvds101@gmail.com>
2023-04-18 15:54:45 +1200

    removed unused variables

--- src/main/java/nz/ac/auckland/se281/HomePolicy.java
+++ src/main/java/nz/ac/auckland/se281/HomePolicy.java
@@ -5,12 +5,10 @@ import nz.ac.auckland.se281.Main.PolicyType;
 public class HomePolicy extends Policy {
 
   private String address;
-  private Boolean rental;
 
   public HomePolicy(PolicyType type, int sum, int policyId, String address, Boolean rental) {
     super(type, sum, policyId);
     this.address = address;
-    this.rental = rental;
     if (rental == false) {
       this.basePremium = sum * 0.01;
     } else {
